c80fe04cd60177422b8e845aa6d27f2a89b7b21b
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-05-19 15:13:50 -0400

    Configure meta information and molecule

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,7 +12,25 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-@pytest.mark.parametrize("x", [True])
-def test_packages(host, x):
-    """Run a dummy test, just to show what one would look like."""
-    assert x
+def test_packages(host):
+    """Test that the appropriate packages were installed."""
+    distribution = host.system_info.distribution
+    if distribution == "fedora":
+        pkgs = ["make", "rpm-build", "amazon-ssm-agent"]
+    elif distribution == "debian" or distribution == "ubuntu" or distribution == "kali":
+        pkgs = ["make", "binutils", "amazon-ssm-agent"]
+    elif distribution == "amzn":
+        pkgs = ["amazon-ssm-agent"]
+    else:
+        # We don't support this distribution
+        assert False
+    packages = [host.package(pkg) for pkg in pkgs]
+    installed = [package.is_installed for package in packages]
+    assert len(pkgs) != 0
+    assert all(installed)
+
+
+@pytest.mark.parametrize("service", ["amazon-ssm-agent"])
+def test_services(host, service):
+    """Test that the expected services were enabled."""
+    assert host.service(service).is_enabled
